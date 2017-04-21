f415054c467e1a6f84e2dab65535fd2d3b2903a7
Kent Sommer <kent.sommer13@gmail.com>
2017-04-21 16:41:09 +0900

    Added download script

---
+++ download_weights_and_datasets.sh
@@ -0,0 +1,9 @@
+cd trained
+wget 'https://github.com/kentsommer/pytorch-value-iteration-networks/releases/download/1.0/vin_8x8.pth'
+wget 'https://github.com/kentsommer/pytorch-value-iteration-networks/releases/download/1.0/vin_16x16.pth'
+wget 'https://github.com/kentsommer/pytorch-value-iteration-networks/releases/download/1.0/vin_28x28.pth'
+cd ../dataset
+wget 'https://github.com/kentsommer/pytorch-value-iteration-networks/releases/download/1.0/gridworld_8x8.npz'
+wget 'https://github.com/kentsommer/pytorch-value-iteration-networks/releases/download/1.0/gridworld_16x16.npz'
+wget 'https://github.com/kentsommer/pytorch-value-iteration-networks/releases/download/1.0/gridworld_28x28.npz'
+cd ..
